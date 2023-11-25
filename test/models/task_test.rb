ff6860d186a6c89e7e545705863c372d10e7cb6b
igim11 <migicastro@yahoo.com>
2023-11-25 13:00:24 +0800

    edited task tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,9 +2,15 @@ require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
   
-  test "should not save task without description" do
+  test "should not save task without category ID" do
     task = Task.new
     task.description = 'Finish project'
+    assert_not task.save, 'saved the task without a category ID'
+  end
+
+  test "should not save task without a description" do
+    task = Task.new
+    task.category_id = 1
     assert_not task.save, 'saved the task without a description'
   end
 end
